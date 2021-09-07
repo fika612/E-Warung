752a077402d2d7e0807ef92fa57106a1396e1e1c
Radela shafa Kirana <radelakirana775@gmail.com>
2021-09-07 12:04:28 +0700

    Initial commit

---
+++ app/src/main/java/com/example/e_warung/MainActivity2.kt
@@ -0,0 +1,110 @@
+package com.example.e_warung
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.ImageButton
+import android.widget.TextView
+import android.widget.Toast
+
+class MainActivity2 : AppCompatActivity() {
+    private lateinit var kurang1 : ImageButton
+    private lateinit var kurang2 : ImageButton
+    private lateinit var plus1: ImageButton
+    private lateinit var plus2: ImageButton
+    private lateinit var jum1: TextView
+    private lateinit var jum2: TextView
+    private lateinit var totalBayar : TextView
+    private lateinit var jumlahtotal : TextView
+    private var jumlah1 : Int = 0
+    private var jumlah2 : Int = 0
+    private var jumTotal: Int = 0
+    private var totalBayar1 : Int = 0
+    private var totalBayar2 : Int = 0
+    private var totBay : Int = 0
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main2)
+        kurang1 = findViewById(R.id.imageButton4)
+        kurang2 = findViewById(R.id.imageButton6)
+        plus1 = findViewById(R.id.imageButton5)
+        plus2 = findViewById(R.id.imageButton7)
+        jum1 = findViewById(R.id.textView17)
+        jum2 = findViewById(R.id.textView27)
+        jumlahtotal = findViewById(R.id.textView26)
+        totalBayar = findViewById(R.id.textView29)
+
+    }
+
+        fun setMins(view: View) {
+            jumlah1=jum1.text.toString().toInt()
+            if (jumlah1==0) {
+                Toast.makeText(this,"minum pesanan 1", Toast.LENGTH_SHORT).show()
+            }else{
+                jumlah1=jumlah1 - 1
+                jum1. setText(jumlah1.toString())
+                this.setJumTotalBeli()
+                this.setJumTotalBayar()
+
+            }
+        }
+    fun setPlus(view: View) {
+        jumlah1 = jum1.text.toString().toInt()
+        if (jumlah1 == 5) {
+            Toast.makeText( this, "minum pesanan 5", Toast.LENGTH_SHORT).show()
+        } else {
+            jumlah1 = jumlah1 + 1
+            jum1.setText(jumlah1.toString())
+            this.setJumTotalBeli()
+            this.setJumTotalBayar()
+        }
+    }
+    fun setMins2(view: View) {
+        jumlah2 = jum2.text.toString().toInt()
+        if (jumlah2 == 0) {
+            Toast.makeText( this, "minum pesanan 1", Toast.LENGTH_SHORT).show()
+        } else {
+            jumlah2 = jumlah2 - 1
+            jum2.setText(jumlah2.toString())
+            this.setJumTotalBeli()
+            this.setJumTotalBayar()
+        }
+    }
+    fun setPlus2(view: View){
+        jumlah2 = jum2.text.toString().toInt()
+        if(jumlah2==5){
+            Toast.makeText(this, "minimum pesanan 5", Toast.LENGTH_LONG).show()
+        }
+        else{
+            jumlah2 = jumlah2+1
+            jum2.setText(jumlah2.toString())
+            this.setJumTotalBeli()
+            this.setJumTotalBayar()
+        }
+    }
+    private fun setJumTotalBeli(){
+        jumlah2=jum2.text.toString().toInt()
+        jumlah1=jum1.text.toString().toInt()
+        jumTotal=jumlah1+jumlah2
+        jumlahtotal.setText(jumTotal.toString())
+    }
+    private fun setJumTotalBayar(){
+        jumlah2 = jum2.text.toString().toInt()
+        jumlah1 = jum1.text.toString().toInt()
+        totalBayar2 = jumlah2*22000
+        totalBayar1 = jumlah1*20000
+        totBay = totalBayar1+totalBayar2
+        totalBayar.setText(totBay.toString())
+
+    }
+    fun setBack(view: View) {
+        val intent = Intent(this, MainActivity::class.java)
+        startActivity(intent)
+    }
+}
+
+
+
+
